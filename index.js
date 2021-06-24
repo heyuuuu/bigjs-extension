cb43de820b37e40d9ff949efe827d6b2d2c3c81d
heyu-home <694959859@qq.com>
2021-06-24 23:52:57 +0800

    修正负数计算

--- index.js
+++ index.js
@@ -1,55 +1,66 @@
 import Big from "big.js"
 
+const isNum = /^{?([\d\.\-]+)}?$/
+
+function peelon(val){
+    return '{' + val + '}'
+}
+
+function peeloff(val){
+    const [_,rawdata] = (val||"").match(isNum) || ['0',val]
+    return rawdata
+}
+
 // 计算器
 function calculate(evaluate){
-	const resourceArr = evaluate.match(/([\d\.]+)|(\+|\-|\*|\/)/g)
+	const resourceArr = evaluate.match(/({\-?[\d\.]+})|([\d\.]+)|(\+|\-|\*|\/)/g)
 	if(resourceArr){
 		// 性能优化，避免单个数值进行计算
 		if(resourceArr.length === 1){
 			return evaluate
 		}
 		resourceArr.map((val,index) => {
-			const prveVal = resourceArr[index - 2]
+			const prveVal = peeloff(resourceArr[index - 2])
 			const operator = resourceArr[index - 1]
-			if(/^[\d\.]+$/.test(prveVal)){
+			if(isNum.test(prveVal)){
 				if(operator == "*" || operator == "/"){
 					resourceArr[index - 2] = "_"
 					resourceArr[index - 1] = "_"
 				}
 				if(operator == "*"){
-					resourceArr[index] = new Big(prveVal).times(val).toString()
+                    resourceArr[index] = new Big(prveVal).times(peeloff(val)).toString()
 				}
 				if(operator == "/"){
-					resourceArr[index] = new Big(prveVal).div(val).toString()
+                    resourceArr[index] = new Big(prveVal).div(peeloff(val)).toString()
 				}
 			}
 		})
 		const resourceArr1 = resourceArr.filter(v => v !== "_")
 		resourceArr1.map((val,index) => {
-			const prveVal = resourceArr1[index - 2]
+			const prveVal = peeloff(resourceArr1[index - 2])
 			const operator = resourceArr1[index - 1]
-			if(/^[\d\.]+$/.test(prveVal)){
+			if(isNum.test(prveVal)){
 				if(operator == "+" || operator == "-"){
 					resourceArr1[index - 2] = "_"
 					resourceArr1[index - 1] = "_"
 				}
 				if(operator == "+"){
-					resourceArr1[index] = new Big(prveVal).plus(val).toString()
+                    resourceArr1[index] = new Big(prveVal).plus(peeloff(val)).toString()
 				}
 				if(operator == "-"){
-					resourceArr1[index] = new Big(prveVal).minus(val).toString()
+                    resourceArr1[index] = new Big(prveVal).minus(peeloff(val)).toString()
 				}
 			}
 		})
-		return resourceArr1.pop()
+		return peelon(resourceArr1.pop())
 	}
-	return ''
+	return '0'
 }
 
 // 精度计算
 function precision(evaluate,decimal = 2){
 	const recursion = (evaluate) => {
-		const result = evaluate.replace(/\(([^\(\)]+\))/,(_,expression)=> {
+		const result = evaluate.replace(/\(([^\(\)]+\))/,(expression)=> {
 			return calculate(expression)
 		})
 		if(result.indexOf('(') !== -1 ){
@@ -58,7 +69,7 @@ function precision(evaluate,decimal = 2){
 			return calculate(result)
 		}
 	}
-	const result = recursion(evaluate)
+	const result = peeloff(recursion(evaluate))
 	return new Big(result).toFixed(decimal)
 }
 
